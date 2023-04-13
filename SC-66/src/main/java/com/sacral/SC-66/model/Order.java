26ab26c47a482d165f08cd7a28d753547713dffd
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-13 13:23:47 +0530

    initial commit

---
+++ SC-66/src/main/java/com/sacral/SC-66/model/Order.java
@@ -0,0 +1,167 @@
+package com.sacral.SC-66.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Order {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    private String accountNumber;
+    private String orderReferenceNumber;
+    private String orderDate;
+    private String fromDate;
+    private String toDate;
+    private String product;
+    private String country;
+    private String status;
+    private String exportDate;
+    private String exportNumber;
+    private String orderReference;
+    private String date;
+    private String time;
+    private String buyerIPAddress;
+    private String buyerName;
+    private String buyerEmail;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getOrderReferenceNumber() {
+        return orderReferenceNumber;
+    }
+
+    public void setOrderReferenceNumber(String orderReferenceNumber) {
+        this.orderReferenceNumber = orderReferenceNumber;
+    }
+
+    public String getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(String orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public String getFromDate() {
+        return fromDate;
+    }
+
+    public void setFromDate(String fromDate) {
+        this.fromDate = fromDate;
+    }
+
+    public String getToDate() {
+        return toDate;
+    }
+
+    public void setToDate(String toDate) {
+        this.toDate = toDate;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getExportDate() {
+        return exportDate;
+    }
+
+    public void setExportDate(String exportDate) {
+        this.exportDate = exportDate;
+    }
+
+    public String getExportNumber() {
+        return exportNumber;
+    }
+
+    public void setExportNumber(String exportNumber) {
+        this.exportNumber = exportNumber;
+    }
+
+    public String getOrderReference() {
+        return orderReference;
+    }
+
+    public void setOrderReference(String orderReference) {
+        this.orderReference = orderReference;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getBuyerIPAddress() {
+        return buyerIPAddress;
+    }
+
+    public void setBuyerIPAddress(String buyerIPAddress) {
+        this.buyerIPAddress = buyerIPAddress;
+    }
+
+    public String getBuyerName() {
+        return buyerName;
+    }
+
+    public void setBuyerName(String buyerName) {
+        this.buyerName = buyerName;
+    }
+
+    public String getBuyerEmail() {
+        return buyerEmail;
+    }
+
+    public void setBuyerEmail(String buyerEmail) {
+        this.buyerEmail = buyerEmail;
+    }
+}
